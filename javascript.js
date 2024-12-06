2025ef4ede113ea4542695f826ee5c86c39b7cc6
bravekarthick <karthickwebdeveloper83@gmail.com>
2024-12-06 21:20:35 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,420 @@
+// alert('aravindh12345!"£$$')
+// alert(12345)
+// alert(6<7)
+// alert(5>5)
+// alert(BigInt(12345))
+// alert(11.23)
+
+// var car = 123
+// var car = 321
+// var car = 'karthick'
+
+// alert(car) 
+
+
+// let van = 10.23
+// let van = 21.20
+// alert(van)
+
+// const bike = 11.2123
+// const bike1 = 12.2131
+// alert(bike)
+// alert(bike1)
+
+// var kar = prompt('enter your name')
+// alert(kar)
+
+// let age = prompt('enter your age')
+// let age = prompt('enter your age')
+// alert(age)
+
+// operator -> Assignment,Arithmetic,Logical,Conditional,Bitwsie Operator
+// 1. Assignment Operator -> =
+
+// let myname = 'karthick'
+// console.log(myname);
+
+// arithmetic operator -> +,-,*,/,and %
+
+// console.log(6+6+6);
+// console.log(6-6);
+// console.log(6*6);
+// console.log(6/6);
+// console.log(7%6);
+
+
+// Logical Operator -> >,<,>=,<=,==,===
+
+// console.log(5>=5);
+// console.log(5<=5);
+// console.log('5'===5);
+// console.log('5'==5);
+
+// console.log(5+'karthick')
+
+// Conditional Operator -> &&,||,!
+
+// console.log((8==88) && (5>=5));
+// console.log((8==88) || (5>5));
+// console.log(!(8==88) && (5>=5));
+
+
+// Conditional Operator -> &,|,!
+// console.log((8==8) & (5>=5));
+// console.log((8==88) | (5>5));
+// console.log(!(8==88) & (5>=5));
+
+
+// console.log((8==8) && (5<5));
+// console.log((7==7) || (7>7));
+// console.log(!(8==88) && (5<5));
+
+
+// console.log((8==8) & (5<5));
+// console.log((7==7) | (7>7));
+// console.log(!(8==88) & (5<5));
+
+
+// for,for-of,for-in ,while,do-while
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index*5);
+// }
+
+// let myname= prompt("enter your name")
+// console.log(myname.length);
+
+// for (let index = 0; index < myname.length; index++) {
+//     const element = myname[index];
+//     console.log(element);
+    
+    
+// }
+
+// for (let index = 1; index <=10; index++) {
+//     console.log(index);   
+// }
+
+
+
+// let index = 100;
+// while (index < myname.length) {
+//     const karthick = myname[index];
+//     console.log(karthick);
+//     index++
+// }
+// while (index <= 10) {
+//         console.log(index);
+//         index++
+// }
+
+// while (index <= 10) {
+//         console.log(index);
+//         index++
+// }
+// for (let index = 0; index < myname.length; index++) {
+//     const karthick = myname[index];
+//     console.log(karthick);
+// }
+// let index = 0
+// while (index < myname.length) {
+//     const karthick = myname[index];
+//         console.log(karthick);
+//         index++
+// }
+
+// for (let index = 0; index <= 10; index++) {
+//         console.log(index);
+
+// } 
+// let index = 0
+// while (index <= 10) {
+//     console.log(index);
+//     index++
+// }
+
+// do {
+//     console.log(index);
+//     index++
+// }while (index <= 10)
+
+
+// Array, String & DOM
+// let myname = ['karthick','wajid',232,12.23,true]
+// let myname = []
+// myname[0]= 'karthick'
+// myname[2]= 'wajid'
+// myname[1]= true
+
+// console.log(myname);
+
+// let myname = ['aravindh',true,2]
+
+// unshift - add the 6 at first position
+// myname.unshift(6)
+
+// push - add the 6 at last position
+// myname.push(6)
+
+// join - join the data as single data
+// console.log(myname.join('-'));
+
+// pop - remove the last data
+// myname.pop()
+
+// shift - remove the first data
+// myname.shift()
+
+// splice - add the data inbetween depending upon position
+// myname.splice(1,1,'karthcik')
+
+// concat - add the data two array data
+// console.log(myname.concat(myname1))
+
+// flat - multiple arrag convert into single array
+// myname.flat([34,3,4,5,6,67])
+// console.log(myname.flat());
+
+
+// let myname =['aravindh','santhosh','naveen']
+// myname.push(23)
+// console.log(myname.join('-'));
+// myname.pop()
+// myname.shift()
+// myname.splice(1,0,'karthick')
+// myname.push([12,34,45,465,67])
+// console.log(myname.flat());
+
+
+// let myname = 'karthick is'
+
+
+// console.log(myname.indexOf('a'));
+
+// console.log(myname.trim());
+// console.log(myname.trimEnd());
+// console.log(myname.trimStart());
+// console.log(myname.toLowerCase());
+// console.log(myname.toUpperCase());
+// console.log(myname.charAt(5));
+// console.log(myname.match('ic'));
+// console.log(myname.replace('is',22));
+
+// console.log(myname.trim());
+// console.log(myname.trimEnd());
+// console.log(myname.trimStart());
+// console.log(myname.toLowerCase());
+// console.log(myname.toLocaleUpperCase());
+// console.log(myname.charAt(1));
+// console.log(myname.match('th'));
+// console.log(myname.replace('is',22));
+
+
+// Date -> set and get method
+// DOM -> display the data or get the data from the browser.
+// DOM -> Document Object Modeling or Manipulation
+
+
+
+// setinterval and settimeout
+
+// setInterval(() => {
+//     let totaldataandtime=new Date();
+//     console.log(totaldataandtime.setHours(1)+';'+totaldataandtime.setMinutes(30)+';'+totaldataandtime.setSeconds(20));
+    
+//     console.log(totaldataandtime);
+    // let gggcall = document.getElementById("ggg")
+    // gggcall.innerHTML = totaldataandtime.getHours()+':'+totaldataandtime.getMinutes()+':'+totaldataandtime.getSeconds()
+    
+      
+// }, 1000);
+
+
+// setInterval(() => {
+//     let karthick=new Date();
+//     console.log(karthick.setDate(10)+';'+karthick.setMonth(0)+'1'+karthick.setFullYear(2002));
+    
+//     console.log(karthick);
+    
+//     // let karthick1 = document.getElementById('aravindh')
+//     // console.log(karthick);
+    
+//     // karthick1.innerHTML = karthick.getDate()+'/'+karthick.getFullYear()+'/'+Number(karthick.getMonth()+1)
+// }, 1000);
+
+// temperal dead zone;
+// console.log(karthick);
+
+
+// var karthick= new Date();
+
+
+// DOM 
+
+// let tag = document.getElementsByClassName('aravindh')[1]
+// tag.innerHTML = 'karthick'
+
+// let tag = document.querySelector('.aravindh')
+// tag.innerHTML = 'naveen'
+
+// let myname1 = prompt('enter your name')
+
+// for (let index = 0; index < myname1.length; index++) {
+//     const element = myname1[index];
+//     console.log(element);    
+// }
+
+
+// for (const element of myname1) {
+//     console.log(element);    
+// }
+
+
+// let details = {
+//     myname:'karthick',
+//     age:24
+// }
+// console.log(details);
+// for (const key in details) {
+//     console.log(details[key]);
+   
+// }
+
+// let detail = {
+//     myname: 'sanjay',
+//     age:23
+// }
+// console.log(detail);
+
+// for (const key in detail) {
+//   console.log(detail[key]);
+  
+// }
+
+// AJAX -> Asynchronous Javascript And XML
+
+// let httpcreate = new XMLHttpRequest();
+// httpcreate.open('GET','karthick.txt');
+// httpcreate.onload = function () {
+//     console.log(this.responseText);
+//     let gggtag = document.getElementById('ggg')
+//     gggtag.innerHTML = this.responseText
+    
+// }
+// httpcreate.send()
+
+
+// let website = new XMLHttpRequest();
+// website.open('GET','karthcik.json')
+// website.onload= function() {
+//     let removestring = JSON.parse(this.responseText)
+//     console.log(removestring);
+//     let aravindhtag = document.querySelectorAll('#ggg')[0];
+//     for (const key in removestring) {
+//       console.log(removestring.myname);
+      
+//       aravindhtag.innerHTML =removestring.myname 
+//     }
+    
+    
+// // }
+// website.send()
+
+// let website = new XMLHttpRequest();
+// website.open('GET','karthcik.json')
+// website.onload= function () {
+//     let removestring = JSON.parse(this.responseText)
+//     console.log(removestring);
+//     let aravindhtag = document.querySelectorAll('#ggg')[0]
+//     for (const key in removestring) {
+//         console.log(removestring.myname);
+//         aravindhtag.innerHTML = removestring.myname
+        
+//         }
+
+    
+
+
+// }
+// website.send()
+// let moredetails = [
+//     {
+//         myname:'karthick',
+//         myage:20
+//     },
+//     {
+//         myname:'wajid',
+//         myage:26
+//     }
+// ]
+
+
+
+// moredetails.map((dd)=>{
+//     console.log(dd.myname);
+//     console.log(dd.myage);
+
+
+    
+// })
+
+// let personal = [
+//     {
+//         myname:'aravindh',
+//           myage:20
+//     }
+//  ]        
+// console.log(personal[0].myname);
+// https://jsonplaceholder.typicode.com/todos/1
+//  personal.map((dd)=>{
+//     console.log(dd.myname);
+    
+//  }) 
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos/").then((dd)=>dd.json()).then((output)=>{
+//     console.log(output);
+    
+// })
+
+
+// let task = new XMLHttpRequest();
+// httpcreate.open('GET','task.json');
+// httpcreate.onload = function () {
+//     console.log(this.responseText);
+//     let gggtag = document.getElementById('ggg')
+//     titletag.innerHTML = this.responseText
+// }
+// task.send()
+
+
+// let task = new XMLHttpRequest();
+// task.open('GET', 'task.json'); 
+// task.onload = function () {
+//     console.log(this.responseText);
+//     let gggtag = document.getElementById('ggg');
+//     gggtag.innerHTML = this.responseText; 
+// }
+// task.send();
+
+// task=>1
+
+// let task = new XMLHttpRequest();
+// task.open('GET','task.json')
+// task.onload= function() {
+//     let removestring = JSON.parse(this.responseText)
+//     console.log(removestring);
+//     let aravindhtag = document.querySelectorAll('#ggg')[0];
+//     for (const key in removestring) {
+//       console.log(removestring.title);
+      
+//       aravindhtag.innerHTML =removestring.title 
+//     }
+    
+    
+// }
+// task.send()
+
+
+task =>2
+
